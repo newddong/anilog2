d7a005419a3c187447c960de9453f68b211a5478
lky <newddong@hanmail.net>
2022-02-18 12:38:08 +0900

    [기영]StageBar 중복 import 오류 수정

--- src/component/templete/user/UserPasswordCheck.js
+++ src/component/templete/user/UserPasswordCheck.js
@@ -9,7 +9,6 @@ import StageBar from 'Molecules/info/Stagebar';
 import PasswordChecker from 'Organism/form/PasswordChecker';
 import {stagebar_style} from 'Organism/style_organism copy';
 import {login_style, btn_style, temp_style, progressbar_style, userPasswordCheck} from 'Templete/style_templete';
-import StageBar from 'Root/component/molecules/info/Stagebar';
 
 // 각각 뷰에 컴포넌트 삽입시 style의 첫번째 index 삭제할 것. 두번째 index는 상.하 간격 style이라서 이 컴포넌트에만 해당 됨.
 //ex) 변경 전: <View style={[btn_style.btn_w654, findAccount_style.btn_w654]}>   변경 후:  <View style={[findAccount_style.btn_w654]}>

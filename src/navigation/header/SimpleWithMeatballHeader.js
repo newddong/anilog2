4847bb5aa07a47500dfc27b1c040b33871478344
kwon sangwoo <sw.kwon@pine-partners.com>
2022-03-11 02:41:18 +0900

    [상우] PR 전 미세 조정작업

--- src/navigation/header/SimpleWithMeatballHeader.js
+++ src/navigation/header/SimpleWithMeatballHeader.js
@@ -14,102 +14,12 @@ import {setProtectRequestStatus} from 'Root/api/protectapi';
 export default SimpleWithMeatballHeader = ({navigation, route, options, back}) => {
 	const isWriter = userGlobalObject.userInfo._id == route.params.writer;
 	// console.log('route.params / SimpleWithMeatballHeader : ', route.params.request_object);
-	const g = {
-		__v: 0,
-		_id: '6226f807269db4bdc7628b89',
-		protect_animal_id: {
-			__v: 0,
-			_id: '6226f687269db4bdc7628b6f',
-			protect_act_applicants: [],
-			protect_animal_belonged_shelter_id: '62220ed4477a1c991fd47cf7',
-			protect_animal_estimate_age: '1개월',
-			protect_animal_neutralization: 'unknown',
-			protect_animal_photo_uri_list: [
-				'https://pinetreegy.s3.ap-northeast-2.amazonaws.com/upload/1646720646917_24893246-4742-47C0-A4D0-D0D53F5C8DB3.jpg',
-				'https://pinetreegy.s3.ap-northeast-2.amazonaws.com/upload/1646720646971_10DA8154-1D4E-4FD0-9E50-BF95359F0DF3.jpg',
-				'https://pinetreegy.s3.ap-northeast-2.amazonaws.com/upload/1646720647099_A584AEE4-8ABE-413E-A32B-8A2A905BD86E.jpg',
-				'https://pinetreegy.s3.ap-northeast-2.amazonaws.com/upload/1646720647174_EB642F5B-0250-487B-837A-3E5E3BBE6AEF.jpg',
-			],
-			protect_animal_protect_request_id: '6226f807269db4bdc7628b89',
-			protect_animal_protector_discussion_id: [],
-			protect_animal_rescue_date: '2022-03-01T00:00:00.000Z',
-			protect_animal_rescue_location: '아카베',
-			protect_animal_sex: 'female',
-			protect_animal_species: '개',
-			protect_animal_species_detail: '믹스견',
-			protect_animal_status: 'rescue',
-			protect_animal_weight: 3,
-		},
-		protect_animal_species: '개',
-		protect_animal_species_detail: '믹스견',
-		protect_request_comment_count: 0,
-		protect_request_content: 'Dsd',
-		protect_request_date: '2022-03-08T06:30:31.999Z',
-		protect_request_favorite_count: 0,
-		protect_request_hit: 0,
-		protect_request_is_delete: false,
-		protect_request_photos_uri: [
-			'https://pinetreegy.s3.ap-northeast-2.amazonaws.com/upload/1646720646917_24893246-4742-47C0-A4D0-D0D53F5C8DB3.jpg',
-			'https://pinetreegy.s3.ap-northeast-2.amazonaws.com/upload/1646720646971_10DA8154-1D4E-4FD0-9E50-BF95359F0DF3.jpg',
-			'https://pinetreegy.s3.ap-northeast-2.amazonaws.com/upload/1646720647099_A584AEE4-8ABE-413E-A32B-8A2A905BD86E.jpg',
-			'https://pinetreegy.s3.ap-northeast-2.amazonaws.com/upload/1646720647174_EB642F5B-0250-487B-837A-3E5E3BBE6AEF.jpg',
-		],
-		protect_request_status: 'rescue',
-		protect_request_title: 'Ds',
-		protect_request_update_date: '2022-03-08T06:30:31.999Z',
-		protect_request_writer_id: {
-			__v: 0,
-			_id: '62220ed4477a1c991fd47cf7',
-			pet_family: [],
-			shelter_address: {brief: '서울 중랑구 용마산로 194', detail: '22013'},
-			shelter_delegate_contact_number: '01001096450003',
-			shelter_foundation_date: '2014-08-01T00:00:00.000Z',
-			shelter_homepage: 'Htttp:dd12',
-			shelter_name: '탄지보호소',
-			shelter_type: 'public',
-			type: 'UserObject',
-			user_agreement: {
-				is_donation_info: false,
-				is_location_service_info: false,
-				is_marketting_info: false,
-				is_over_fourteen: false,
-				is_personal_info: false,
-				is_service: false,
-			},
-			user_denied: false,
-			user_email: 'Lanad01@kakao.com',
-			user_follow_count: 1,
-			user_follower_count: 0,
-			user_interests: {
-				interests_activity: [Array],
-				interests_beauty: [Array],
-				interests_food: [Array],
-				interests_health: [Array],
-				interests_location: [Array],
-			},
-			user_introduction: '',
-			user_is_verified_email: false,
-			user_is_verified_phone_number: false,
-			user_my_pets: [],
-			user_name: '탄지보호소',
-			user_nickname: '탄지보호소',
-			user_password: 'tkddn123',
-			user_phone_number: '01001096450003',
-			user_profile_uri: 'https://pinetreegy.s3.ap-northeast-2.amazonaws.com/upload/1646399188505_07B57E46-3AFE-4DEF-AC34-974489A43703.jpg',
-			user_register_date: '2022-03-04T13:06:28.612Z',
-			user_type: 'shelter',
-			user_update_date: '2022-03-04T13:06:28.612Z',
-			user_upload_count: 5,
-		},
-	};
-
-	//요청게시글 ‘rescue’,'complete',’rainbowbridge’,’discuss’), //항목 추가 필요
-
-	//보호동물 (‘rescue’,’adopt’,’protect’,’rainbowbridge’,’discuss’),
 
 	const onPressChangeProtectRequestStatus = () => {
 		Modal.close();
 		setTimeout(() => {
+			//요청게시글 ‘rescue’,'complete',’rainbowbridge’,’discuss’), //항목 추가 필요
+			//보호동물 (‘rescue’,’adopt’,’protect’,’rainbowbridge’,’discuss’),
 			Modal.popSelectScrollBoxModal([PROTECT_REQUEST_STATUS], '', selectedItem => {
 				const request_obj = route.params.request_object;
 				let selectedStatus = '';

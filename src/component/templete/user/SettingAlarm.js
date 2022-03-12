352ba299eb8e0d869f2e703d14f346f38619d5b0
이재현 <jhlsuper@gmail.com>
2022-03-12 21:38:43 +0900

    [재현] 공개 설정, 알림 설정 api 연결 전체 공개 버튼 rerender 관련 작업필요

--- src/component/templete/user/SettingAlarm.js
+++ src/component/templete/user/SettingAlarm.js
@@ -7,6 +7,7 @@ import {userLogout} from 'Root/api/userapi';
 import DP from 'Root/config/dp';
 import userGlobalObject from 'Root/config/userGlobalObject';
 import OnOffSwitch from 'Molecules/select/OnOffSwitch';
+import {getNotice, updateNotice} from 'Root/api/notice';
 import {
 	FAVORITE_PROTECT_STATUS_CHANGE_ALRAM,
 	FOLLWER_NEW_POST_ALRAM,
@@ -17,95 +18,223 @@ import {
 	TAG_OR_FOLLOW_ALRAM,
 } from 'Root/i18n/msg';
 export default SettingAlarm = ({route}) => {
-	const onSwtichOn = () => {};
+	const [alarm, setAlarm] = React.useState();
+	const [loading, setLoading] = React.useState(true);
+	const [apiPost, setApiPost] = React.useState(false);
+	React.useEffect(() => {
+		getNotice(
+			{},
+			noticeObject => {
+				var temp = noticeObject.msg[0];
 
-	//계정 공개 여부 변경 Switch Off
-	const onSwtichOff = () => {};
+				delete temp._id;
+				delete temp.notice_update_date;
+				delete temp.notice_user_id;
+				delete temp.__v;
+				// console.log('temp', temp);
+				setAlarm(noticeObject.msg[0]);
+				setLoading(false);
+			},
 
-	return (
-		<ScrollView>
-			<View style={styles.container}>
-				<View style={styles.everyAlarmContainer}>
-					<View style={{flexDirection: 'row'}}>
-						<View style={{width: 550 * DP}}>
-							<Text style={[txt.noto32b, {color: APRI10}]}>전체 알림</Text>
+			err => {
+				console.log('er', err);
+			},
+		);
+	}, []);
+	React.useEffect(() => {
+		console.log('alarm', alarm, apiPost);
+		updateNotice(
+			{...alarm},
+			callback => {
+				console.log('updateNotice', callback);
+			},
+
+			err => {
+				console.log('er', err);
+			},
+		);
+	}, [alarm]);
+
+	const onSwtichAll = () => {
+		//전체 알람이 true이면
+		if (alarm.notice_all) {
+			setAlarm(prevState => ({
+				...prevState,
+				notice_all: false,
+				notice_newfollower: false,
+				notice_favorite_protect_request: false,
+				notice_pet_vaccination: false,
+				notice_my_post: false,
+				notice_comment_on_my_post: false,
+				notice_tag_follower: false,
+				notice_my_applicant: false,
+				notice_alarm: false,
+			}));
+			// setApiPost(!apiPost);
+		} else {
+			setAlarm(prevState => ({
+				...prevState,
+				notice_all: true,
+				notice_newfollower: true,
+				notice_favorite_protect_request: true,
+				notice_pet_vaccination: true,
+				notice_my_post: true,
+				notice_comment_on_my_post: true,
+				notice_tag_follower: true,
+				notice_my_applicant: true,
+				notice_alarm: true,
+			}));
+			// setApiPost(!apiPost);
+		}
+	};
+	const onSwtichFollower = () => {
+		setAlarm(prevState => ({
+			...prevState,
+			notice_newfollower: !prevState.notice_newfollower,
+		}));
+		// setApiPost(!apiPost);
+	};
+	const onSwtichProtectStatus = () => {
+		setAlarm(prevState => ({
+			...prevState,
+			notice_favorite_protect_request: !prevState.notice_favorite_protect_request,
+		}));
+		// setApiPost(!apiPost);
+	};
+	const onSwtichPetVaccine = () => {
+		setAlarm(prevState => ({
+			...prevState,
+			notice_pet_vaccination: !prevState.notice_pet_vaccination,
+		}));
+		setApiPost(!apiPost);
+	};
+	const onSwtichMyPost = () => {
+		setAlarm(prevState => ({
+			...prevState,
+			notice_my_post: !prevState.notice_my_post,
+		}));
+		setApiPost(!apiPost);
+	};
+	const onSwtichMyPostComment = () => {
+		setAlarm(prevState => ({
+			...prevState,
+			notice_comment_on_my_post: !prevState.notice_comment_on_my_post,
+		}));
+		// setApiPost(!apiPost);
+	};
+	const onSwtichMyTagFollow = () => {
+		setAlarm(prevState => ({
+			...prevState,
+			notice_tag_follower: !prevState.notice_tag_follower,
+		}));
+		// setApiPost(!apiPost);
+	};
+	const onSwtichApplyStatus = () => {
+		setAlarm(prevState => ({
+			...prevState,
+			notice_my_applicant: !prevState.notice_my_applicant,
+		}));
+		setApiPost(!apiPost);
+	};
+	const onSwtichNotice = () => {
+		setAlarm(prevState => ({
+			...prevState,
+			notice_alarm: !prevState.notice_alarm,
+		}));
+		setApiPost(!apiPost);
+	};
+	if (loading) {
+		return <View></View>;
+	} else {
+		return (
+			<ScrollView>
+				<View style={styles.container}>
+					<View style={styles.everyAlarmContainer}>
+						<View style={{flexDirection: 'row'}}>
+							<View style={{width: 550 * DP}}>
+								<Text style={[txt.noto32b, {color: APRI10}]}>전체 알림</Text>
+							</View>
+							<OnOffSwitch onSwtichOff={onSwtichAll} onSwtichOn={onSwtichAll} />
 						</View>
-						<OnOffSwitch onSwtichOff={onSwtichOff} onSwtichOn={onSwtichOn} />
+					</View>
+					<View style={styles.serviceAlarmContainer}>
+						<Text style={[txt.noto32b, {color: GRAY10}]}>서비스별 알림</Text>
+						<View style={[styles.alarmDetailEachContainer, {marginTop: 30 * DP}]}>
+							<View style={{flexDirection: 'row'}}>
+								<View style={[{width: 550 * DP}, {flexDirection: 'row'}, {alignItems: 'center'}]}>
+									<Text style={[txt.noto28, {color: GRAY10}]}>{FOLLWER_NEW_POST_ALRAM}</Text>
+								</View>
+								<OnOffSwitch default={alarm.notice_newfollower || ''} onSwtichOff={onSwtichFollower} onSwtichOn={onSwtichFollower} />
+							</View>
+						</View>
+						<View style={[styles.alarmDetailEachContainer, {marginTop: 24 * DP}]}>
+							<View style={{flexDirection: 'row'}}>
+								<View style={[{width: 550 * DP}, {flexDirection: 'row'}, {alignItems: 'center'}]}>
+									<Text style={[txt.noto28, {color: GRAY10}]}>{FAVORITE_PROTECT_STATUS_CHANGE_ALRAM}</Text>
+								</View>
+								<OnOffSwitch
+									default={alarm.notice_favorite_protect_request || ''}
+									onSwtichOff={onSwtichProtectStatus}
+									onSwtichOn={onSwtichProtectStatus}
+								/>
+							</View>
+						</View>
+						<View style={[styles.alarmDetailEachContainer, {marginTop: 24 * DP}]}>
+							<View style={{flexDirection: 'row'}}>
+								<View style={[{width: 550 * DP}, {flexDirection: 'row'}, {alignItems: 'center'}]}>
+									<Text style={[txt.noto28, {color: GRAY10}]}>{PET_VACCIN_DATE_ALRAM}</Text>
+								</View>
+								<OnOffSwitch default={alarm.notice_pet_vaccination || ''} onSwtichOff={onSwtichPetVaccine} onSwtichOn={onSwtichPetVaccine} />
+							</View>
+						</View>
+					</View>
+					<View style={styles.activityAlarmContainer}>
+						<Text style={[txt.noto32b, {color: GRAY10}]}>내 활동 알림</Text>
+						<View style={[styles.alarmDetailEachContainer, {marginTop: 30 * DP}]}>
+							<View style={{flexDirection: 'row'}}>
+								<View style={[{width: 550 * DP}, {flexDirection: 'row'}, {alignItems: 'center'}]}>
+									<Text style={[txt.noto28, {color: GRAY10}]}>{MY_POST_ALRAM}</Text>
+								</View>
+								<OnOffSwitch default={alarm.notice_my_post || ''} onSwtichOff={onSwtichMyPost} onSwtichOn={onSwtichMyPost} />
+							</View>
+						</View>
+						<View style={[styles.alarmDetailEachContainer, {marginTop: 24 * DP}]}>
+							<View style={{flexDirection: 'row'}}>
+								<View style={[{width: 550 * DP}, {flexDirection: 'row'}, {alignItems: 'center'}]}>
+									<Text style={[txt.noto28, {color: GRAY10}]}>{MY_POST_COMMENT_ALRAM}</Text>
+								</View>
+								<OnOffSwitch default={alarm.notice_comment_on_my_post || ''} onSwtichOff={onSwtichMyPostComment} onSwtichOn={onSwtichMyPostComment} />
+							</View>
+						</View>
+						<View style={[styles.alarmDetailEachContainer, {marginTop: 24 * DP}]}>
+							<View style={{flexDirection: 'row'}}>
+								<View style={[{width: 550 * DP}, {flexDirection: 'row'}, {alignItems: 'center'}]}>
+									<Text style={[txt.noto28, {color: GRAY10}]}>{TAG_OR_FOLLOW_ALRAM}</Text>
+								</View>
+								<OnOffSwitch default={alarm.notice_tag_follower || ''} onSwtichOff={onSwtichMyTagFollow} onSwtichOn={onSwtichMyTagFollow} />
+							</View>
+						</View>
+						<View style={[styles.alarmDetailEachContainer, {marginTop: 24 * DP}]}>
+							<View style={{flexDirection: 'row'}}>
+								<View style={[{width: 550 * DP}, {flexDirection: 'row'}, {alignItems: 'center'}]}>
+									<Text style={[txt.noto28, {color: GRAY10}]}>{MY_APPLY_STATUS_CHANGE_ALRAM}</Text>
+								</View>
+								<OnOffSwitch default={alarm.notice_my_applicant || ''} onSwtichOff={onSwtichApplyStatus} onSwtichOn={onSwtichApplyStatus} />
+							</View>
+						</View>
+					</View>
+					<View style={styles.noticeAlarmContainer}>
+						<View style={{flexDirection: 'row'}}>
+							<View style={[{width: 550 * DP}, {flexDirection: 'row'}, {alignItems: 'center'}]}>
+								<Text style={[txt.noto32b, {color: GRAY10}]}>공지 알림</Text>
+							</View>
+							<OnOffSwitch default={alarm.notice_alarm || ''} onSwtichOff={onSwtichNotice} onSwtichOn={onSwtichNotice} />
+						</View>
 					</View>
 				</View>
-				<View style={styles.serviceAlarmContainer}>
-					<Text style={[txt.noto32b, {color: GRAY10}]}>서비스별 알림</Text>
-					<View style={[styles.alarmDetailEachContainer, {marginTop: 30 * DP}]}>
-						<View style={{flexDirection: 'row'}}>
-							<View style={[{width: 550 * DP}, {flexDirection: 'row'}, {alignItems: 'center'}]}>
-								<Text style={[txt.noto28, {color: GRAY10}]}>{FOLLWER_NEW_POST_ALRAM}</Text>
-							</View>
-							<OnOffSwitch onSwtichOff={onSwtichOff} onSwtichOn={onSwtichOn} />
-						</View>
-					</View>
-					<View style={[styles.alarmDetailEachContainer, {marginTop: 24 * DP}]}>
-						<View style={{flexDirection: 'row'}}>
-							<View style={[{width: 550 * DP}, {flexDirection: 'row'}, {alignItems: 'center'}]}>
-								<Text style={[txt.noto28, {color: GRAY10}]}>{FAVORITE_PROTECT_STATUS_CHANGE_ALRAM}</Text>
-							</View>
-							<OnOffSwitch onSwtichOff={onSwtichOff} onSwtichOn={onSwtichOn} />
-						</View>
-					</View>
-					<View style={[styles.alarmDetailEachContainer, {marginTop: 24 * DP}]}>
-						<View style={{flexDirection: 'row'}}>
-							<View style={[{width: 550 * DP}, {flexDirection: 'row'}, {alignItems: 'center'}]}>
-								<Text style={[txt.noto28, {color: GRAY10}]}>{PET_VACCIN_DATE_ALRAM}</Text>
-							</View>
-							<OnOffSwitch onSwtichOff={onSwtichOff} onSwtichOn={onSwtichOn} />
-						</View>
-					</View>
-				</View>
-				<View style={styles.activityAlarmContainer}>
-					<Text style={[txt.noto32b, {color: GRAY10}]}>내 활동 알림</Text>
-					<View style={[styles.alarmDetailEachContainer, {marginTop: 30 * DP}]}>
-						<View style={{flexDirection: 'row'}}>
-							<View style={[{width: 550 * DP}, {flexDirection: 'row'}, {alignItems: 'center'}]}>
-								<Text style={[txt.noto28, {color: GRAY10}]}>{MY_POST_ALRAM}</Text>
-							</View>
-							<OnOffSwitch onSwtichOff={onSwtichOff} onSwtichOn={onSwtichOn} />
-						</View>
-					</View>
-					<View style={[styles.alarmDetailEachContainer, {marginTop: 24 * DP}]}>
-						<View style={{flexDirection: 'row'}}>
-							<View style={[{width: 550 * DP}, {flexDirection: 'row'}, {alignItems: 'center'}]}>
-								<Text style={[txt.noto28, {color: GRAY10}]}>{MY_POST_COMMENT_ALRAM}</Text>
-							</View>
-							<OnOffSwitch onSwtichOff={onSwtichOff} onSwtichOn={onSwtichOn} />
-						</View>
-					</View>
-					<View style={[styles.alarmDetailEachContainer, {marginTop: 24 * DP}]}>
-						<View style={{flexDirection: 'row'}}>
-							<View style={[{width: 550 * DP}, {flexDirection: 'row'}, {alignItems: 'center'}]}>
-								<Text style={[txt.noto28, {color: GRAY10}]}>{TAG_OR_FOLLOW_ALRAM}</Text>
-							</View>
-							<OnOffSwitch onSwtichOff={onSwtichOff} onSwtichOn={onSwtichOn} />
-						</View>
-					</View>
-					<View style={[styles.alarmDetailEachContainer, {marginTop: 24 * DP}]}>
-						<View style={{flexDirection: 'row'}}>
-							<View style={[{width: 550 * DP}, {flexDirection: 'row'}, {alignItems: 'center'}]}>
-								<Text style={[txt.noto28, {color: GRAY10}]}>{MY_APPLY_STATUS_CHANGE_ALRAM}</Text>
-							</View>
-							<OnOffSwitch onSwtichOff={onSwtichOff} onSwtichOn={onSwtichOn} />
-						</View>
-					</View>
-				</View>
-				<View style={styles.noticeAlarmContainer}>
-					<View style={{flexDirection: 'row'}}>
-						<View style={[{width: 550 * DP}, {flexDirection: 'row'}, {alignItems: 'center'}]}>
-							<Text style={[txt.noto32b, {color: GRAY10}]}>공지 알림</Text>
-						</View>
-						<OnOffSwitch onSwtichOff={onSwtichOff} onSwtichOn={onSwtichOn} />
-					</View>
-				</View>
-			</View>
-		</ScrollView>
-	);
+			</ScrollView>
+		);
+	}
 };
 
 const styles = StyleSheet.create({

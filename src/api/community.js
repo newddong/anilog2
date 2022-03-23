173296a6044b548dd6f210a925d8f0db8541008c
hjs2738 <88875088+hjs2738@users.noreply.github.com>
2022-03-23 21:29:54 +0900

    이용약관 생성, 불러오기 추가 ,  커뮤니티 s3 경로 변경 함수의 파리미터 수정 (#64)
    * [hjs] 봉사활동 인원수 추가
    
    * [hjs] 봉사활동 인원수 추가
    
    * [hjs] 봉사활동 참여 인원의 수락 여부 상태 변경
    
    * [hjs] API - 쪽지함 기능 함수 추가
    
    * [hjs] API - 이용약관 생성, 불러오기 추가
    
    * [hjs] API - 커뮤니티 s3 경로 변경 함수의 파리미터 수정
    
    Co-authored-by: desk <js.hwang@pine-partners.com>

--- src/api/community.js
+++ src/api/community.js
@@ -3,7 +3,7 @@ import {apiController, apiFormController} from './apiController';
 /**
  * 로컬정보를 s3 정보로 변경
  * @param {object} params
- * @param {string} params.s3path - 로컬 경로
+ * @param {string} params.s3path_uri - 로컬 경로 uri
  * @param {function} callback - API응답처리 콜백
  * @param {(errmsg:string)=>void} errcallback - 에러처리 콜백
  */

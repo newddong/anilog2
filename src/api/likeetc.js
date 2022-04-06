458e4c85d4b44df60dcad334b4f2250af20d5c17
hjs2738 <88875088+hjs2738@users.noreply.github.com>
2022-04-06 11:51:16 +0900

    API - 좋아요&취소 (피드외에  '좋아요'가 필요한 모든 컬렉션 적용) (#80)
    * [hjs] 봉사활동 인원수 추가
    
    * [hjs] 봉사활동 인원수 추가
    
    * [hjs] 봉사활동 참여 인원의 수락 여부 상태 변경
    
    * [hjs] API - 쪽지함 기능 함수 추가
    
    * [hjs] API - 커뮤니티 불러오기 글의 community_type에서 타입 추가 (all 추가)
    
    * [hjs] API - 커뮤니티 게시물 업데이트 및 삭제 함수 추가
    
    * [hjs] API - 좋아요&취소 (피드외에  '좋아요'가 필요한 모든 컬렉션 적용)
    
    Co-authored-by: desk <js.hwang@pine-partners.com>

---
+++ src/api/likeetc.js
@@ -0,0 +1,14 @@
+import {apiController, apiFormController} from './apiController';
+
+/**
+ * 좋아요&취소 (피드외에 신규 추가되는 모든 게시물을 다룸)
+ * @param {object} params
+ * @param {string} params.collectionName - 타겟 컬렉션 이름
+ * @param {string} params.post_object_id - 게시물 object_id
+ * @param {string} params.is_like - 좋아요를 설정할때는 true, 취소할때는 false를 보내 좋아요를 설정/취소
+ * @param {function} callback - API응답처리 콜백
+ * @param {(errmsg:string)=>void} errcallback - 에러처리 콜백
+ */
+export async function likeEtc(params, callback, errcallback) {
+	apiController('/likeetc/likeEtc', arguments);
+}

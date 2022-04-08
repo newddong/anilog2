b03021612b735b435d30052adcb1fedef0850a91
hjs-desk <js.hwang@pine-partners.com>
2022-04-08 14:41:31 +0900

    [hjs] API - 특정 유저의 즐겨찾기 목록 조회 (피드를 제외한 타 게시물)

--- src/api/favoriteetc.js
+++ src/api/favoriteetc.js
@@ -12,3 +12,15 @@ import {apiController, apiFormController} from './apiController';
 export async function favoriteEtc(params, callback, errcallback) {
 	apiController('/favoriteetc/favoriteEtc', arguments);
 }
+
+/**
+ * 특정 유저의 즐겨찾기 목록 조회 (피드를 제외한 타 게시물)
+ * @param {object} params
+ * @param {string} params.userobject_id - 게시물 object_id
+ * @param {string} params.collectionName - 타겟 컬렉션 이름
+ * @param {function} callback - API응답처리 콜백
+ * @param {(errmsg:string)=>void} errcallback - 에러처리 콜백
+ */
+export async function getFavoriteEtcListByUserId(params, callback, errcallback) {
+	apiController('/favoriteetc/getFavoriteEtcListByUserId', arguments);
+}

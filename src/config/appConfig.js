0a1dba846d2714379d7229d66fb2866e7fc5d800
lky <newddong@hanmail.net>
2022-06-24 12:22:02 +0900

    [기영]AppConfig 추가(버전 문구, 모드 설정관련 객체 추가)

---
+++ src/config/appConfig.js
@@ -0,0 +1,9 @@
+export const DEV = 111;
+export const RELEASE = 222;
+export const STAGING = 333;
+
+const appConfig = {
+    mode:RELEASE,
+    version: '0.0.1 ver(staging, 6-23 13:56)'
+}
+export default appConfig;
